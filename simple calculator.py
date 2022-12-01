0eba8694d2d5e056d21b5cb135bd7cf0a1402107
HelloMati <matimotof@gmail.com>
2022-12-01 21:49:54 +0000

    Initial Commit

---
+++ simple calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input("> Enter the first number: "))
+operator = (input("> Enter '+', '-', '*' or '/' "))
+num2 = int(input("> Enter the second number: "))
+
+if operator == '+':
+    answer = num1 + num2
+
+if operator == '-':
+    answer = num1 - num2
+
+if operator == '*':
+    answer = num1 * num2
+
+if operator == '/':
+    answer = num1 / num2
+
+print("> The answer is " + str(answer))
