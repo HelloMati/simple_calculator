a34c09941c2c1a4924d814dbfeb01ca272422d08
HelloMati <matimotof@gmail.com>
2022-12-02 00:49:15 +0000

    Closed while loop

--- simple calculator.py
+++ simple calculator.py
@@ -1,7 +1,7 @@
 while True:
-    num1 = int(input("> Enter the first number: "))
-    operator = (input("> Enter '+', '-', '*' or '/' "))
-    num2 = int(input("> Enter the second number: "))
+    num1 = int(input("Enter the first number: "))
+    operator = (input("Enter '+', '-', '*' or '/' "))
+    num2 = int(input("Enter the second number: "))
 
     if operator == '+':
         answer = num1 + num2
@@ -15,4 +15,11 @@ while True:
     if operator == '/':
         answer = num1 / num2
 
-    print("> The answer is " + str(answer))
+    print("The answer is " + str(answer))
+
+    exit = (input("Enter 'q' to exit or press enter to continue "))
+
+    if exit == 'q':
+        break
+
+    print()
